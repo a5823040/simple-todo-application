91780edb1d53e2529efc40d5f8acba68d2136493
a5823040 <a5823040@aoyama.jp>
2024-06-21 16:44:40 +0900

    aaa

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ function addTask(title) {
     const task = { isDone: false }
 
     // やることのタイトルを設定(このままだと、すべてのタイトルがHello Worldになってしまう！)
-    task.title = 
-    
+    task.title = title
 
     // 以降は無視して良い
     tasks.push(task);
